105611a8b2b996bcbab89bf5ac36d22e7116994c
wrjacqmein <wrjacqmein@gmail.com>
2018-03-24 08:42:40 -0400

    Updated json to best practices available at https://hodgkins.io/best-practices-with-packer-and-windows
    Updated vm-guest tools to utilize attach method instead of upload.

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -28,10 +28,8 @@ goto :done
 
 :virtualbox
 
-move /Y C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
-cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
-cmd /c for %%i in (C:\Windows\Temp\virtualbox\cert\vbox*.cer) do C:\Windows\Temp\virtualbox\cert\VBoxCertUtil add-trusted-publisher %%i --root %%i
-cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
+cmd /c for %%i in (D:\cert\vbox*.cer) do D:\cert\VBoxCertUtil add-trusted-publisher %%i --root %%i
+cmd /c D:\VBoxWindowsAdditions.exe /S
 goto :done
 
 :parallels
